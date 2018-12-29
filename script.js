678740c12d06ea68865ce443957867240c660bd6
Justin Schulz <justin.schulz@gmail.com>
2018-12-29 11:23:36 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+//Spell #1
+
+$("#oculus").click(function(){
+ $("#broken-glasses").hide();
+ $("#fixed-glasses").show();
+});
+
+
+
+//Spell #2
+
+$("#finite").click(function(){
+ $("#quidditch").slideToggle();
+});
+
+
+
+//Spell #3
+
+$("#obliviate").click(function(){
+ $("#grangers").fadeOut();
+ $("#crying").show();
+});
+
+
+
